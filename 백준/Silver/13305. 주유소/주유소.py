41e1d4af2c2e815a38387015a78fe92898d3b3da
Youngjae Son <yjson991203@naver.com>
2023-09-06 06:23:26 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 46584 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,23 +1,13 @@
 n = int(input())
 a = 0
-
 dist = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
-price.pop()
-n -= 1
+min = price[0]
 
-while True:
-    if len(price) == 0:
-        break
-    cheap = price.index(min(price))
-    for i in range(cheap, n):
-        a += price[cheap]*dist[i]
-        
-    for i in range(cheap, n):
-        del dist[cheap]
-        del price[cheap]
-        
-    n = cheap
+for i in range(n-1):
+    if min > price[i]:
+        min = price[i]
+    a += min * dist[i]
     
 print(a)
